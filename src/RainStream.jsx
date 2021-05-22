58582ab53085fd4cf6dda3e4d71438901d2da632
thushanK <ktwimalaweera@gmail.com>
2021-05-22 14:34:00 +0530

    make randomly select letters

--- src/RainStream.jsx
+++ src/RainStream.jsx
@@ -1,3 +1,19 @@
+//characters set
+const VALID_CHARS = `abcdefghijklmnopqrstuvwxyz0123456789$+-*/=%"'#&_(),.;:?!\\|{}<>[]^~`;
+
+const MIN_STREAM_SIZE = 15;
+const MAX_STREAM_SIZE = 50;
+
+const getRandInRange = (min, max) =>
+	Math.floor(Math.random() * (max - min)) + min;
+
+const getRandChar = () =>
+	VALID_CHARS.charAt(Math.floor(Math.random() * VALID_CHARS.length));
+
+const getRandStream = () =>
+	new Array(getRandInRange(MIN_STREAM_SIZE, MAX_STREAM_SIZE))
+		.fill().map(_ => getRandChar());
+
 const RainStream = () => {
     return (
         <div
@@ -11,7 +27,12 @@ const RainStream = () => {
                 textShadow: '0px 0px 8px rgbs(32, 194, 14, 0.8)',
                 fontSize: 20,
             }}>
-                <a>testing</a>
+                {getRandStream().map(char =>(
+                    <a
+                        style={{margin: -12,}}>
+                            {char}
+                        </a>
+                        ))}
         </div>
     );
 };
